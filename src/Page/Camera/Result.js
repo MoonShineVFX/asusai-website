9ee4cddea83b0713dcb9c2ee561c0abea8774ff5
wood.wu <kilokingw@gmail.com>
2023-11-20 18:26:24 +0800

    wip layout0.0.3

--- src/Page/Camera/Result.js
+++ src/Page/Camera/Result.js
@@ -63,7 +63,7 @@ function Result({open ,handleOpen,renderedResult,username}) {
                 </Link>
                 <Link to='/' className=" relative mt-2 w-3/4 mx-auto" onClick={handleOpen}>
                   <div className='sample-heading-3 w-full h-full absolute top-0 z-10   opacity-0 hover:opacity-100 cursor-pointer  '></div>
-                  <div className='text-white text-sm font-normal bg-gray-800/40 px-5 py-2 border  border-white/30 flex items-center justify-center text-center ' >回到首頁</div>
+                  <div className='text-white text-sm font-normal bg-[#FF0050] px-5 py-2 border  border-white/30 flex items-center justify-center text-center ' >回到首頁</div>
                 </Link>
               </div>
 
