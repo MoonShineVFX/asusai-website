5a6c26f67d7d55c5cfdbe0864b1992912f35f5e0
wood.wu <kilokingw@gmail.com>
2023-12-01 17:34:03 +0800

    wip mblayout0.0.3

--- src/Page/Camera/FrontPage.js
+++ src/Page/Camera/FrontPage.js
@@ -52,14 +52,14 @@ function FrontPage({handleClick}) {
               animate={{ opacity: 1 , y:isHovered ? 10:0}}
               exit={{ opacity: 0,y:40 }}
               transition={{ duration: 1.5 }}
-              src="https://moonshine.b-cdn.net/msweb/asusaicamera/images/cover_mb.png?width=400" alt="p01" className='max-w-full w-full img_ref ' />
+              src="https://moonshine.b-cdn.net/msweb/asusaicamera/images/cover_mb.png?width=400" alt="p01" className='max-w-full w-full img_ref my-12 ' />
               :
               <motion.picture
                 initial={{ opacity: 0, y:  40 }}
                 animate={{ opacity: 1 , y:isHovered ? 10:0}}
                 exit={{ opacity: 0,y:40 }}
                 transition={{ duration: 1.5 }}
-                className='mx-auto'
+                className='mx-auto '
               >
                 <source
                   src='https://moonshine.b-cdn.net/msweb/asusaicamera/images/cover.png?format=webp&width=1280'
